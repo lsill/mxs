658c88e9e5a4f3e0d5d5a1ca51cc95f5e157ea24
lsill <lishuai@taiyouxi.cn>
2021-01-18 10:53:45 +0800

    websocket 增加消息handler(?是否需要确定采用消息队列)

---
+++ gamex/api/websocket/iface/imsghandler.go
@@ -0,0 +1,9 @@
+package iface
+
+type IMsgHandle interface {
+	DoMsgHandler(requset IRequest)
+	AddRouter(MsgId uint32, router IRouter) // 为消息添加具体的处理逻辑
+	StarWorkerPool()                        // 启动worker工作池
+	SendMsgToTaskQueue(request IRequest)    // 将消息交给TaskQueue,又worker进行处理
+}
+
